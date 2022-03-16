db085b941030f68d3c1337bae4879ebb14b758d1
cquir <christian.quirouette@ryerson.ca>
2022-03-16 08:38:41 -0400

    clean how data is saved (save in subdirectories, store args as header) save training loss and accuracy over entire training dataset each epoch

--- train.py
+++ train.py
@@ -1,27 +1,28 @@
+import subprocess
 import numpy
 import torch
 import load
 import nn
+import os
 
 args = dict(
-    h = 16,
+    h = 64,
     lr = 0.001,
     batch_size = 64,
     log_interval = 1000,
-    epochs = 30
+    epochs = 20
 )
 
-#lrs = numpy.tile([1e-1,1e-2,1e-3,1e-4,1e-5],5)
-#hs = numpy.repeat([16,32,64,128,256],5)
+lrs = numpy.tile([1e-2,1e-3,1e-4,1e-5,1e-6],5)
+batch_sizes = numpy.repeat([],5)
+hs = numpy.repeat([16,32,64,128,256],5)
 
-lrs = [1e-2,1e-3,1e-4,1e-5]
-h = 256
-
-#for lr,h in zip(lrs,hs):
-for lr in lrs:
+for lr,batch_size in zip(lrs,batch_sizes):
     
-    print('Learning rate: {:.1e}, h: {}\n'.format(lr,h))
-    args['lr'] = lr; args['h'] = h
+    args['lr'] = lr; args['batch_size'] = batch_size
+
+    print('Learning rate: {:.1e}, batch size: {}, h: {}\n'.format(
+        args['lr'],args['batch_size'],args['h']))
 
     train_loader,val_loader = load.load_data(args)
 
@@ -35,17 +36,14 @@ for lr in lrs:
             optimizer.zero_grad()
             output = model(data)
             loss = criterion(output,label.unsqueeze(1))
-            pred = torch.round(torch.sigmoid(output))
-            correct = (pred.squeeze(1) == label).sum().item()
             loss.backward()
             optimizer.step()
             if batch_idx % args['log_interval'] == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}, Accuracy: {:.1f}%'.format(
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]'.format(
                     epoch, batch_idx*len(data),len(train_loader.dataset),
-                    100.*batch_idx/len(train_loader),loss.item(),100.*correct/len(data)))
-        return loss.item()
+                    100.*batch_idx/len(train_loader)))
 
-    def test(test_loader):
+    def test(test_loader,dataset_label):
         model.eval()
         test_loss = 0
         correct = 0
@@ -55,19 +53,27 @@ for lr in lrs:
             pred = torch.round(torch.sigmoid(output))
             correct += (pred.squeeze(1) == label).sum().item()
         test_loss *= args['batch_size']/len(test_loader.dataset) 
-        print('\nTest set: Average loss: {:.4f}, Accuracy: {:.1f}%\n'.format(
-            test_loss,100.*correct/len(test_loader.dataset)))
+        print('{} set: Average loss: {:.4f}, Accuracy: {:.1f}%'.format(
+            dataset_label,test_loss,100.*correct/len(test_loader.dataset)))
         return test_loss, correct/len(test_loader.dataset)
 
     train_loss = numpy.zeros((args['epochs'],))
+    train_accuracy = numpy.zeros_like(train_loss)
     val_loss = numpy.zeros_like(train_loss)
     val_accuracy = numpy.zeros_like(train_loss)
     epochs = range(1,args['epochs']+1)
     for epoch in epochs:
-        train_loss[epoch-1] = train(epoch)
-        val_loss[epoch-1], val_accuracy[epoch-1] = test(val_loader)
+        train(epoch)
+        print('\n')
+        train_loss[epoch-1], train_accuracy[epoch-1] = test(train_loader,'Train')
+        val_loss[epoch-1], val_accuracy[epoch-1] = test(val_loader,'Validation')
+        print('\n')
     
-    s =  'lr_{:.1e}_h_{}'.format(args['lr'],args['h'])
-    numpy.savetxt('data/train_loss_{}.dat'.format(s),train_loss)
-    numpy.savetxt('data/val_loss_{}.dat'.format(s),val_loss)
-    numpy.savetxt('data/val_accuracy_{}.dat'.format(s),val_accuracy)
+    # save data
+    s =  'lr_{:.1e}_batch_size_{}_h_{}'.format(args['lr'],args['batch_size'],args['h'])
+    if s not in os.listdir('data/'):
+        subprocess.call(f'mkdir data/{s}',shell=True)
+    header = 'args = {}'.format(repr(args))
+    numpy.savetxt(f'data/{s}/train_loss_{s}.dat',train_loss)
+    numpy.savetxt(f'data/{s}/val_loss_{s}.dat',val_loss)
+    numpy.savetxt(f'data/{s}/val_accuracy_{s}.dat',val_accuracy)
