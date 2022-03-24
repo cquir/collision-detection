2c48b98d36373a0102e58dfdd90388ac734309b1
cquir <christian.quirouette@ryerson.ca>
2022-03-24 18:02:32 -0400

    modify to specify what subdirectory to use

--- load.py
+++ load.py
@@ -2,9 +2,9 @@ import numpy
 import torch
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self,label):
-        X = numpy.loadtxt('data/X%s.dat'%label).astype(numpy.float32)
-        Y = numpy.loadtxt('data/Y%s.dat'%label).astype(numpy.float32)
+    def __init__(self,label,bdir,subdir):
+        X = numpy.loadtxt(f'{bdir}/data/datasets/{subdir}/X{label}.dat').astype(numpy.float32)
+        Y = numpy.loadtxt(f'{bdir}/data/datasets/{subdir}/Y{label}.dat').astype(numpy.float32)
         self.data = (X,Y)
     def __len__(self):
         return len(self.data[1])
@@ -12,8 +12,9 @@ class Dataset(torch.utils.data.Dataset):
         return (self.data[0][idx],self.data[1][idx])
 
 def load_data(args):
-    train_data = Dataset('train')
-    val_data = Dataset('validation')
+    bdir = '/home/cquir/Documents/collision-detection'
+    train_data = Dataset('train',bdir,args['subdir'])
+    val_data = Dataset('validation',bdir,args['subdir'])
     train_loader = torch.utils.data.DataLoader(train_data,batch_size=args['batch_size'],shuffle=True)
     val_loader = torch.utils.data.DataLoader(val_data,batch_size=args['test_batch_size'],shuffle=True)
     return train_loader,val_loader
