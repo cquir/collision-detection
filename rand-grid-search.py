c5bdf427977b74abc6ab95528850dc4a7a75cd79
cquir <christian.quirouette@ryerson.ca>
2022-03-24 18:05:50 -0400

    add some options to change the number of hidden layers and set a random number seed

--- rand-grid-search.py
+++ rand-grid-search.py
@@ -1,31 +1,36 @@
 import subprocess
 import numpy
+import torch
 import train
 import load
 import sys
 import os
 
 args = dict(
-    h = 64,
-    lr = 0.001,
-    momentum = 0.5,
-    batch_size = 64,
+    subdir = 'low-level-features',
     test_batch_size = 1000,
     log_interval = 1000,
     epochs = 100,
-    patience = 10
+    patience = 10,
+    i = 2*3+2*4,
+    dropout = False,
+    seed = 0
 )
 
-for i in range(50):
+torch.manual_seed(args['seed'])
+numpy.random.seed(args['seed'])
+
+N = 100
+for i in range(N):
     # update + print hyperparameters
+    args['hidden_layers'] = int(sys.argv[1])
     idx = numpy.nonzero(numpy.random.multinomial(n=1,pvals=4*[0.25]))[0][0]
     args['h'] = [50,100,200,500][idx]
     args['lr'] = 10**numpy.random.uniform(low=-5,high=-1)
-    args['momentum'] = numpy.random.uniform()
-    idx = numpy.nonzero(numpy.random.multinomial(n=1,pvals=4*[0.25]))[0][0]
+    idx = numpy.nonzero(numpy.random.multinomial(n=1,pvals=5*[0.2]))[0][0]
     args['batch_size'] = [32,64,128,256,512][idx]
-    print('h: {}, learning rate: {:.4e}, momentum: {:.5f}, batch size: {}\n'.format(
-        args['h'],args['lr'],args['momentum'],args['batch_size'])) 
+    print('[{}/{}] hidden layers: {}, h: {}, learning rate: {:.4e}, batch size: {}\n'.format(
+        i,N,args['hidden_layers'],args['h'],args['lr'],args['batch_size'])) 
     sys.stdout.flush()
 
     # load data + train model
@@ -33,12 +38,13 @@ for i in range(50):
     model, train_loss, val_loss, val_accuracy = train.evaluate_model(args,train_loader,val_loader)
 
     # save results
-    s =  'h_{}_lr_{:.1e}_momentum_{:.5f}_batch_size_{}'.format(
-        args['h'],args['lr'],args['momentum'],args['batch_size']) 
+    subdir = args['subdir']
+    s =  'hidden_layers_{}_h_{}_lr_{:.1e}_batch_size_{}'.format(
+        args['hidden_layers'],args['h'],args['lr'],args['batch_size']) 
     if s not in os.listdir('data/'):
-        subprocess.call(f'mkdir data/{s}',shell=True)
+        subprocess.call(f'mkdir data/results/{subdir}/{s}',shell=True)
     header = 'args = {}'.format(repr(args))
-    numpy.savetxt(f'data/{s}/train_loss_{s}.dat',train_loss,header=header)
-    numpy.savetxt(f'data/{s}/val_loss_{s}.dat',val_loss,header=header)
-    numpy.savetxt(f'data/{s}/val_accuracy_{s}.dat',val_accuracy,header=header)
-    torch.save(model.state_dict(),f'data/{s}/model_{s}.pt')
+    numpy.savetxt(f'data/results/{subdir}/{s}/train_loss_{s}.dat',train_loss,header=header)
+    numpy.savetxt(f'data/results/{subdir}/{s}/val_loss_{s}.dat',val_loss,header=header)
+    numpy.savetxt(f'data/results/{subdir}/{s}/val_accuracy_{s}.dat',val_accuracy,header=header)
+    torch.save(model.state_dict(),f'data/results/{subdir}/{s}/model_{s}.pt')
