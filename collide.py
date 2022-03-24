ca8ea07d4e8b3a76dacd1f0e48ddbd857316e0db
cquir <christian.quirouette@ryerson.ca>
2022-03-24 17:58:24 -0400

    modify to use quaternions instead of euler angles

--- collide.py
+++ collide.py
@@ -1,11 +1,10 @@
 import scipy.spatial
 import numpy
 
-def collision_detection(pos0vec,pos1vec,r0vec,r1vec):
+def collision_detection(pos0,pos1,q0,q1):
     # rotation vector + position of center of cubes
-    r0 = scipy.spatial.transform.Rotation.from_rotvec(r0vec)
-    r1 = scipy.spatial.transform.Rotation.from_rotvec(r1vec)
-    pos = numpy.array([pos0vec,pos1vec])
+    r0 = scipy.spatial.transform.Rotation.from_quat(q0)
+    r1 = scipy.spatial.transform.Rotation.from_quat(q1)
 
     # corners + normal vectors of cubes (prior to rotation + translation)
     r = 1.; j = numpy.arange(8)
@@ -15,8 +14,8 @@ def collision_detection(pos0vec,pos1vec,r0vec,r1vec):
     ns = numpy.array([n,n])
 
     # rotate then translate corners of cubes
-    cs[0] = r0.apply(cs[0])+pos[0]
-    cs[1] = r1.apply(cs[1])+pos[1]
+    cs[0] = r0.apply(cs[0])+pos0
+    cs[1] = r1.apply(cs[1])+pos1
 
     # rotate normal vectors of cubes
     ns[0] = r0.apply(ns[0])
