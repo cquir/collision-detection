cc3ea158294829760005934a16b11831f7787370
cquir <christian.quirouette@ryerson.ca>
2022-03-27 14:46:04 -0400

    fix bug of not checking seperating planes

--- collide.py
+++ collide.py
@@ -21,9 +21,14 @@ def collision_detection(pos0,pos1,q0,q1):
     ns[0] = r0.apply(ns[0])
     ns[1] = r1.apply(ns[1])
 
+    # calculate normal vectors of the planes defined by the edges of the two cubes
+    x = numpy.concatenate([ns[0],ns[0],ns[0]])
+    y = numpy.tile(ns[1],3).reshape(9,3)
+    ns = numpy.append(ns,numpy.cross(x,y)).reshape(-1,3)
+
     # determine if there is a collision along each normal vector
     collides = []
-    for n in numpy.concatenate(ns):
+    for n in ns:
         prjs = numpy.dot(numpy.concatenate(cs),n)
         pmin = min(prjs[:8]); pmax = max(prjs[:8])
         collides.append(len(prjs[8:][(prjs[8:] >= pmin) & (prjs[8:] <= pmax)]) > 0)
