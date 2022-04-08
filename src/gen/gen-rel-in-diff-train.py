693df392d385b9cf1c7e17e78a0a90a920cfbb51
cquir <christian.quirouette@ryerson.ca>
2022-04-08 12:27:22 -0400

    generate data with relative inputs (relative position,relative quaternion) + different distribution for training data

---
+++ src/gen/gen-rel-in-diff-train.py
@@ -0,0 +1,81 @@
+import scipy.spatial
+import subprocess
+import collide
+import numpy
+import click
+import os
+
+# generate random uniform quaternion
+def uniform_quaternion():
+    [u,v,w] = numpy.random.uniform(0,1,3)
+    a = lambda u,v: numpy.sqrt(1-u)*numpy.sin(2*numpy.pi*v)
+    b = lambda u,v: numpy.sqrt(1-u)*numpy.cos(2*numpy.pi*v)
+    c = lambda u,w: numpy.sqrt(u)*numpy.sin(2*numpy.pi*w)
+    d = lambda u,w: numpy.sqrt(u)*numpy.cos(2*numpy.pi*w)
+    return [a(u,v),b(u,v),c(u,w),d(u,w)]
+
+# generate random position where distance from center is sampled from a normal distribution
+def rnormd():
+    d = numpy.random.normal(loc=1.3957095697470043,scale=0.10287206575906595)
+    inside = False 
+    while not inside:
+        pos = numpy.array([d,0,0])
+        q = uniform_quaternion()
+        r = scipy.spatial.transform.Rotation.from_quat(q)
+        pos = r.apply(pos)
+        x,y,z = pos
+        hl = (numpy.sqrt(3)+1)/2.
+        inside = (-hl <= x <= hl) and (-hl <= y <= hl) and (-hl <= z <= hl)
+    return pos
+
+# generate example comprising of random position + quaternion 
+def example(label,split):
+    hl = (numpy.sqrt(3)+1)/2.
+    pos = numpy.random.uniform(-hl,hl,3)
+    if 'train' in label:
+        if numpy.random.rand() < split:
+            pos = rnormd()
+    q = uniform_quaternion()
+    return pos,q
+
+@click.command()
+@click.option('--Ntot','Ntot',default=1e6,help='Number of examples')
+@click.option('--split','split',default=0.0,help='Split between sampling distributions for training')
+def gen(Ntot,split):
+
+    if not os.path.isdir('../../data/datasets/'):
+        os.mkdir('../../data/datasets/')
+
+    numpy.random.seed(0)
+
+    for dataset,perc in zip([f'train','validation','test'],[0.64, 0.16,0.2]):
+
+        N = int(Ntot*perc)
+
+        X = []; Y = []
+
+        for i in range(int(N)):
+            # generate example
+            pos,q = example(dataset,split)
+            
+            # check if there is a collision 
+            _, _, res = collide.collision_detection([0,0,0],pos,[1,0,0,0],q)
+
+            # add data to arrays to save later
+            X.append(numpy.concatenate((pos,q)))
+            Y.append(int(res))
+
+        # rescale inputs
+        #numpy.savetxt(f'../../data/datasets/X{label}-mean.dat',numpy.mean(X,axis=0))
+        #numpy.savetxt(f'../../data/datasets/X{label}-std.dat',numpy.std(X,axis=0))
+        #X = (X-numpy.mean(X,axis=0))/numpy.std(X,axis=0)
+
+        # save data
+        label = f'rel-in-split-{split}-Ntot-{Ntot:.1e}'
+        if not os.path.isdir(f'../../data/datasets/{label}/'):
+            os.mkdir(f'../../data/datasets/{label}/')
+        numpy.savetxt(f'../../data/datasets/{label}/X{dataset}-{label}.dat',X)
+        numpy.savetxt(f'../../data/datasets/{label}/Y{dataset}-{label}.dat',Y,fmt='%i')
+
+if __name__ == "__main__":
+    gen()
